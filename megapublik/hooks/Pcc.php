eda62e60c29c1695774276d511b1726c2b909577
Razican <devnull@localhost>
2011-04-03 00:20:55 +0200

    Se soluciona un bug en el hook del lenguaje.
    Resulta que no redireccionaba bien en caso de tener una uri con una dirección pero sin lenguaje.
    Además, se ha reescrito el hook con el código de Md5 project 2.0, para integrarlo mejor con CodeIgniter.

--- megapublik/hooks/Pcc.php
+++ megapublik/hooks/Pcc.php
@@ -1,24 +1,25 @@
 <?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
-
 function language()
-{		
+{
+	log_message('debug', 'Language hook initialised.');
 	$CI			=& get_instance();
 
-	log_message('debug', 'Language hook initialised.');
 	$language	= $CI->session->userdata('language');
 	if(($language != $CI->lang->lang()) && ($CI->lang->lang() != ''))
 	{
-		$CI->session->set_userdata('language', $CI->lang->lang());			
+	$CI->session->set_userdata('language', $CI->lang->lang());
 	}
 	else if(($CI->lang->lang() == '') && ($language == ''))
 	{
-		$CI->session->set_userdata('language', $CI->lang->default_lang());			
-		header("Location: " . $CI->config->site_url($CI->lang->default_lang().$CI->uri->uri_string()), TRUE, 302);
+		$CI->session->set_userdata('language', $CI->lang->default_lang());
+		log_message('debug', 'Redirect to '.$CI->lang->default_lang().$CI->uri->uri_string());
+		redirect($CI->lang->default_lang().$CI->uri->uri_string(), 'location', 302);
 	}
 	else if ($CI->lang->lang() == '')
-	{			
-		header("Location: " . $CI->config->site_url($language.$CI->uri->uri_string()), TRUE, 302);
+	{
+		log_message('debug', 'Redirect to '.$language.'/'.$CI->uri->uri_string());
+		redirect($language.'/'.$CI->uri->uri_string(), 'location', 302);
 	}
 }
 
